2a924ec33e186c94089c1f45ce63dae788854892
Takayuki Honda <takka925h@gmail.com>
2024-04-15 14:00:14 +0900

    bp2

--- bp2/OnlineChatMessenger/client.py
+++ bp2/OnlineChatMessenger/client.py
@@ -1,60 +1,32 @@
 import socket
-import time
 import threading
 
-def receive_messages(sock):
-    while True:
-        data, _ = sock.recvfrom(4096)
-        print("\nReceived:", data.decode('utf-8'))
-        print("Message: ", end='', flush=True)  # ユーザーのメッセージ入力プロンプトを再表示
+def tcp_connect(server_ip, server_port, room_name, operation_code):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((server_ip, server_port))
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 例: チャットルーム「MyRoom」に参加
+    room_name_encoded = room_name.encode('utf-8')
+    header = bytes([len(room_name_encoded)]) + bytes([operation_code]) + b'\x00' + bytes([0])
+    sock.sendall(header + room_name_encoded)
 
-server_address = input("Type in the server's address to connect to: ")
-server_port = 9001
+    # サーバからのレスポンス（トークンなど）を受け取る
+    response = sock.recv(1024)
+    print(f"Received: {response.decode('utf-8')}")
+    sock.close()
 
-port = input("Type in the port to bind to: ")
-port = int(port)
+    return response.decode('utf-8')
 
-while True:
-  username = input("Type in your username: ")
-  if len(username) < 255:
-    break
+def udp_send_message(server_ip, server_port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message.encode('utf-8'), (server_ip, server_port))
 
-address = ''
-message = b'Message to send to the client.'
+def main():
+    server_ip = '127.0.0.1'
+    server_port = 9002
+    room_name = "MyRoom"
+    token = tcp_connect(server_ip, server_port, room_name, 1)  # 1: チャットルーム作成, 2: 参加
+    # UDPでメッセージ送信など
 
-initial_message = bytes([len(username)]) + username.encode('utf-8')
-
-# 空の文字列も0.0.0.0として使用できます。
-sock.bind((address,port))
-
-try:
-  print('sending {!r}'.format(message))
-  # サーバへのデータ送信
-  sent = sock.sendto(initial_message, (server_address, server_port))
-  print('Send {} bytes'.format(sent))
-
-  # メッセージ受信用のスレッドを開始
-  receiver_thread = threading.Thread(target=receive_messages, args=(sock,))
-  receiver_thread.daemon = True  # メインプログラムが終了した時にスレッドも終了させる
-  receiver_thread.start()
-
-  while True:
-    sock.settimeout(10)  # タイムアウトを設定
-    message = input("Type in your message: ")
-    if message == "exit":
-      break
-    message = bytes([len(message)]) + message.encode('utf-8')
-    print('sending {!r}'.format(message))
-    sent = sock.sendto(message, (server_address, server_port))
-    print('Send {} bytes'.format(sent))
-
-    # 応答を受信
-    print('waiting to receive')
-    data, server = sock.recvfrom(4096)
-    print('received {!r}'.format(data))
-
-finally:
-  print('closing socket')
-  sock.close()
+if __name__ == "__main__":
+    main()
--- bp2/OnlineChatMessenger/server.py
+++ bp2/OnlineChatMessenger/server.py
@@ -1,31 +1,57 @@
 import socket
+import threading
+import json
 
-# AF_INETを使用し、UDPソケットを作成
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# クライアント情報とチャットルームを管理する辞書
+clients = {}
+chatrooms = {}
 
-# サーバーのIPアドレスとポート番号を指定
-server_address = '0.0.0.0'
-server_port = 9001
-print('starting up on port {}'.format(server_port))
+def handle_tcp_client(conn, addr):
+    while True:
+        header = conn.recv(32)
+        if not header:
+            break  # クライアントが接続を閉じた場合
 
-clients = set()
+        # ヘッダーから情報を解析
+        room_name_size = header[0]
+        operation = header[1]
+        state = header[2]
+        operation_payload_size = int.from_bytes(header[3:32].strip(b'\x00'), 'big')
 
-# ソケットをアドレスにバインド
-sock.bind((server_address, server_port))
+        room_name = conn.recv(room_name_size).decode('utf-8')
+        operation_payload = conn.recv(operation_payload_size).decode('utf-8')
 
-while True:
-  print('\nwaiting to receive message')
-  data, address = sock.recvfrom(4096)
+        if operation == 1:  # 新しいチャットルームを作成
+            token = "unique_token_{}".format(len(chatrooms) + 1)
+            chatrooms[room_name] = {'host': addr, 'token': token}
+            conn.send(token.encode('utf-8'))  # クライアントにトークンを送信
+        elif operation == 2:  # チャットルームに参加
+            if room_name in chatrooms:
+                token = chatrooms[room_name]['token']
+                conn.send(token.encode('utf-8'))  # 参加成功した場合はトークンを送信
+            else:
+                conn.send(b'Error: Room not found')
 
-  print('received {} bytes from {}'.format(len(data), address))
-  print(data)
+def start_tcp_server():
+    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_tcp.bind(('0.0.0.0', 9002))
+    sock_tcp.listen()
 
-  if data:
-        print(f"Received message from {address}")
-        clients.add(address)
-        # 全クライアントにメッセージをブロードキャスト
-        for client in clients:
-            print(f"Sending message to {client}")
-            if client != address:  # 送信元には送らない
-                print(f"Sending message to {client}")
-                sock.sendto(data, client)
+    while True:
+        conn, addr = sock_tcp.accept()
+        thread = threading.Thread(target=handle_tcp_client, args=(conn, addr))
+        thread.start()
+
+def handle_udp_client():
+    sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock_udp.bind(('0.0.0.0', 9001))
+
+    while True:
+        data, addr = sock_udp.recvfrom(4096)
+        # UDPで受け取ったデータの処理。トークン検証など。
+
+if __name__ == "__main__":
+    tcp_thread = threading.Thread(target=start_tcp_server)
+    tcp_thread.start()
+
+    handle_udp_client()
